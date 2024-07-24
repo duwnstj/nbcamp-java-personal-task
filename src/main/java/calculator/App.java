e71d8623e9195df2d0e045a6d7fdd2f921e0f6ec
Robbie <robbie331@>
2024-07-24 20:22:29 +0900

    1-8 요구사항 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -20,7 +20,6 @@ public class App {
             String c = sc.next();
             int result = 0;
 
-
             switch (c) {
                 case "+":
                     result = a + b;
@@ -51,12 +50,19 @@ public class App {
                     break;
             }
             calculationResult.add(result);
-            System.out.println(calculationResult.toString());
             sc.nextLine();
             System.out.println("가장 먼저 저장된 연산 결과를 삭제 하시겠습니까?  (remove 입력시 삭제)");
             String str2 = sc.nextLine();
             if (str2.equals("remove")) {
                 calculationResult.remove(0);
+            }
+            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력시 조회)");
+            String str3 = sc.nextLine();
+            if (str3.equals("inquiry")) {
+                for (int result1 : calculationResult) {
+                    System.out.print("(" + result1 + ")");
+                }
+                System.out.println();
             }
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String src = sc.nextLine();
