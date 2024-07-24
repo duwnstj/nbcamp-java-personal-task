6255ce1c3edb4ed68b9df66b058432f9e021a933
Robbie <robbie331@>
2024-07-24 11:31:44 +0900

    1-1,1-2,1-3,1-4 요구사항 완료
    다시 템플릿 다운 받아서 진행

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,8 +1,58 @@
 package calculator;
 
+import java.util.Scanner;
+
 public class App {
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        while(true) {
+            System.out.print("첫번째 숫자를 입력하세요 : ");
+            int a = sc.nextInt();
+            System.out.print("두번째 숫자를 입력하세요 : ");
+            int b = sc.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요 : ");
+            String c = sc.next();
+            int result = 0;
+
+
+            switch (c) {
+                case "+":
+                    result = a + b;
+                    System.out.println("결과: " + result);
+                    break;
+                case "-":
+                    result = a - b;
+                    System.out.println("결과: " + result);
+                    break;
+                case "*":
+                    result = a * b;
+                    System.out.println("결과: " + result);
+                    break;
+                case "/":
+                    if (b == 0) {
+                        System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    } else {
+                        System.out.println("결과: " + result);
+                    }
+                    result = a / b;
+                    break;
+                case "%":
+                    result = a % b;
+                    System.out.println("결과: " + result);
+                    break;
+                default:
+                    System.out.println("사칙연산을 다시 입력해주세요");
+                    break;
+            }
+            System.out.println("더 계산하시겠습니까? (아무 문자 입력시 더 계산) (exit 입력 시 종료)");
+            String src = sc.next();
+            if(src.equals("exit")){
+                break;
+            }
+
+        }
     }
 }
+
+
