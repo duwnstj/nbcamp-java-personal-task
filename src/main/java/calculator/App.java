b82ca08c40b672ebec4f4330b54b89bd2650fc5c
Robbie <robbie331@>
2024-07-26 14:02:33 +0900

    2-1 요구사항 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
         List<Integer> calculationResult = new ArrayList<>();
 
         Scanner sc = new Scanner(System.in);
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,32 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
 
+    List<Integer> results = new ArrayList<>();
+
+    public int calculate(int a, int b, String c) {
+        int result = 0;
+        try {
+
+            if (c.equals("+")) {
+                result = a + b;
+
+            } else if (c.equals("-")) {
+                result = a - b;
+            } else if (c.equals("*")) {
+                result = a * b;
+            } else if (c.equals("/")) {
+                result = a / b;
+            } else if (c.equals("%")) {
+                result = a % b;
+            }
+            results.add(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 }
