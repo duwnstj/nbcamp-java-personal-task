6c0e8249892f7de3e92fee63c52bea0ddc4b0db7
여준서 <duwnstj12@naver.com>
2024-07-26 19:20:15 +0900

    2-7 요구사항 구현 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -7,41 +7,57 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-
+        List<Double> circle = new ArrayList<>();
         Calculator abc = new Calculator();
+        Calculator abc2 = new Calculator(circle);
 
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            System.out.print("첫번째 숫자를 입력하세요 : ");
-            int num1 = sc.nextInt();
-            System.out.print("두번째 숫자를 입력하세요 : ");
-            int num2 = sc.nextInt();
-            System.out.print("사칙연산 기호를 입력하세요 : ");
-            String operator = sc.next();
+            System.out.println("(1을 누르면 원의 넓이를 구하는 계산기가 실행됩니다.) , " +
+                    "(2를 누르면 사칙연산을 해주는 계산기가 실행됩니다. )");
+            int choose = sc.nextInt();
+            if (choose == 1) {
+                System.out.println("원의 반지름을 입력하세요 : ");
+                int num3 = sc.nextInt();
+                System.out.println("원의 반지름을 입력하세요 : ");
+                int num4 = sc.nextInt();
+                if (num3 != num4) {
+                    System.out.println("반지름은 같아야합니다. 다시 입력해주세요");
+                } else {
+                    System.out.println("원의 넓이 : " + abc2.calculateCircleArea(num3, num4));
+                    System.out.println("저장된 원의 넓이 값들 : " + abc2.getCirResults());
+                }
+            } else {
+                System.out.print("첫번째 숫자를 입력하세요 : ");
+                int num1 = sc.nextInt();
+                System.out.print("두번째 숫자를 입력하세요 : ");
+                int num2 = sc.nextInt();
+                System.out.print("사칙연산 기호를 입력하세요 : ");
+                String operator = sc.next();
 
 
-            sc.nextLine();
-            System.out.println("결과: " + abc.calculate(num1, num2, operator));
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제 하시겠습니까?  (remove 입력시 삭제)");
-            String str2 = sc.nextLine();
+                sc.nextLine();
+                System.out.println("결과: " + abc.calculate(num1, num2, operator));
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제 하시겠습니까?  (remove 입력시 삭제)");
+                String str2 = sc.nextLine();
 
-            if (str2.equals("remove")) {
-                abc.removeResult();
-            }
-            System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력시 조회)");
-            String str3 = sc.nextLine();
-            if (str3.equals("inquiry")) {
-                abc.inquiryResults();
-                System.out.println();
-            }
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String src = sc.nextLine();
+                if (str2.equals("remove")) {
+                    abc.removeResult();
+                }
+                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력시 조회)");
+                String str3 = sc.nextLine();
+                if (str3.equals("inquiry")) {
+                    abc.inquiryResults();
+                    System.out.println();
+                }
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                String src = sc.nextLine();
 
-            if (src.equals("exit")) {
-                break;
+                if (src.equals("exit")) {
+                    break;
+                }
             }
         }
-
     }
 }
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,11 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator {
-
+    static final double circumference = 3.14;
+    // 사칙 연산을 할 때는 원주율의 값이 필요가 없기 때문에 static을 사용하여 클래스 멤버로 만들어준다.
+    // 원주율의 값인 3.14는 절대 바뀌지 않는 상수이기때문에 final을 사용해준다.
+    private List<Double> cirResults;
     private List<Integer> results;
+
 
     public Calculator() {
         this.results = new ArrayList<>();
+    }
+
+    public Calculator(List<Double> cirResults) {
+        this.cirResults = cirResults;
+    }
+
+    public double calculateCircleArea(int r1, int r2) {
+
+        double result2 = r1 * r2 * circumference;
+        cirResults.add(result2);
+        return result2;
     }
 
     public int calculate(int a, int b, String c) {
@@ -50,6 +65,9 @@ public class Calculator {
     }
 
     public List<Integer> getResults() {
+        for (double result2 : cirResults) {
+            System.out.print("(" + result2 + ")");
+        }
         return results;
     }
 
@@ -57,4 +75,11 @@ public class Calculator {
         this.results = results;
     }
 
+    public List<Double> getCirResults() {
+        return cirResults;
+    }
+
+    public void setCirResults(List<Double> cirResults) {
+        this.cirResults = cirResults;
+    }
 }
